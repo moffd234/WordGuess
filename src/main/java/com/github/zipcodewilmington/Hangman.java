db945df687523e4c9245157d1a6bb4ff5683c4b2
moffd234 <moffd234@gmail.com>
2024-02-28 22:29:44 -0500

    Created a way for the Hangman class to choose a random word and store it as a list of chars in a hiddenWord variable

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -8,4 +8,14 @@ package com.github.zipcodewilmington;
  */
 // Daniel Moffett
 public class Hangman {
+    final private String[] words = new String[]{"cat", "dog", "bog", "cut"};
+    private char[] hiddenWord;
+    public Hangman(){
+        // Generates a random number before 0 - 1 then multiplies it by 3 (the max index)
+        int randomIndex = (int) (Math.random() * 4);
+        // Converts the word at the randomIndex to a list of chars and stores it in hiddenWord
+        hiddenWord = words[randomIndex].toCharArray();
+        System.out.println(hiddenWord);
+    }
+
 }
---
+++ src/main/java/com/github/zipcodewilmington/Main.java
@@ -0,0 +1,9 @@
+package com.github.zipcodewilmington;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 0; i < 100; i++) {
+            Hangman game = new Hangman();
+        }
+    }
+}
