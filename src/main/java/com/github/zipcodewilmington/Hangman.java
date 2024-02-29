c427b1f67575f8035fca1888938af30d57f233c6
moffd234 <moffd234@gmail.com>
2024-02-28 22:34:51 -0500

    Made a method that replaced the logic to get the hidden word

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -11,11 +11,16 @@ public class Hangman {
     final private String[] words = new String[]{"cat", "dog", "bog", "cut"};
     private char[] hiddenWord;
     public Hangman(){
+        hiddenWord = makeHiddenWord();
+    }
+
+    private char[] makeHiddenWord(){
         // Generates a random number before 0 - 1 then multiplies it by 3 (the max index)
         int randomIndex = (int) (Math.random() * 4);
-        // Converts the word at the randomIndex to a list of chars and stores it in hiddenWord
-        hiddenWord = words[randomIndex].toCharArray();
+        // Converts the word at the randomIndex to a list of chars and stores it in word
+        char[] word = words[randomIndex].toCharArray();
         System.out.println(hiddenWord);
+        return hiddenWord;
     }
 
 }
